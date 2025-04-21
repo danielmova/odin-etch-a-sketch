c353e7f6507ce2be41606e2772ebcda781b4d75b
danielmova <dataxf@gmail.com>
2025-04-21 13:14:35 -0500

    Implemented Rezise button A while loop and multiple conditions was the trick

--- code.js
+++ code.js
@@ -1,12 +1,13 @@
 pixelsContainerJs = document.querySelector(".pixelsConteiner");
 
-const gridContainer = 500;
-let gridPixelSize = 10;
+const gridContainer = 350;
+let gridPixelSize = 16;
 
 const individualRow = document.createElement("div");
 const individualColumn = document.createElement("div");
 
 function generateGrid(gridSize){
+  pixelsContainerJs.innerHTML = "";
     for (i = 0; i < gridPixelSize; i++) {
         const row = document.createElement("div");
         row.style.display = "flex";
@@ -14,13 +15,24 @@ function generateGrid(gridSize){
         for (j = 0; j < gridPixelSize; j++) {
           const cell = document.createElement("div");
           row.appendChild(cell);
-          cell.style.width = `${(gridContainer/gridPixelSize)}px`;
-          cell.style.height = `${(gridContainer/gridPixelSize)}px`;
-          cell.style.border = "1px solid white";
-          cell.style.display = "flex";
-          cell.style.background = "red";
+          cell.style.width = `${(gridContainer/gridPixelSize-2)}px`;
+          cell.style.height = `${(gridContainer/gridPixelSize -2)}px`;
+          cell.style.border = "1px solid grey";
+          cell.style.background = "white";
+          cell.addEventListener("mouseenter", (event) => {cell.style.background ="blue"});
       }
     }
 }
 
 generateGrid(gridPixelSize);
+
+function changeGridPixelSize(){
+  var answer = prompt("Enter a number between 1 and 100:");
+  while (!/^[0-9]+$/.test(answer) || answer < 1 ||answer >100) {
+      alert("Invalid answer, try again.");
+      answer = prompt("Enter a number between 1 and 100:");
+  }
+  gridPixelSize = answer;
+  generateGrid(gridPixelSize);
+
+}
